e4ac33e1697e19d511883f4176fb5693583e34d6
Karen Camilly <129320903+karencamill@users.noreply.github.com>
2024-08-24 23:55:53 -0300

    Add files via upload

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var val = 100
+console.log(`A velocidade do seu carro é ${val} km/h`)
+if(val > 60 ) {
+    console.log('Você ultrapassou a velocidade permitida. MULTADO!!')
+}
+console.log(`Dirija sempre com sinto de segurança!`)
